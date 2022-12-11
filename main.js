f61499b738f0c8c4452295585afecf045b55adc2
Hacking-NASSA-with-HTML <follow.trend7777@gmail.com>
2022-12-11 19:45:24 +0400

    feat: add one more example of default function param

--- main.js
+++ main.js
@@ -127,3 +127,11 @@ function houseDescriptor([houseColor = 'green', shutterColors = ['red']]) {
 }
 console.log(houseDescriptor(['green']))
 console.log(houseDescriptor(['green', ['white', 'gray', 'pink']]))
+
+// one more example of default function parameters:
+function buildHouse({ floors = 1, color = 'red', walls = 'brick' } = {}) {
+    return `Your house has ${floors} floor(s) with ${color} ${walls} walls.`;
+}
+console.log(buildHouse()); // Your house has 1 floor(s) with red brick walls.
+console.log(buildHouse({})); // Your house has 1 floor(s) with red brick walls.
+console.log(buildHouse({ floors: 3, color: 'yellow' })); // Your house has 3 floor(s) with yellow brick walls.
